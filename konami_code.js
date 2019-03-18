3bf6e34e998b0692d8a4a1e6581a141da9678ff0
mathematical-application-3607 <hannamarie.mehmet@wework.com>
2019-03-18 19:51:44 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -7,3 +7,25 @@ const body = document.querySelector('body')
 body.addEventListener('keydown', e => console.log(e.which))
 
 }
+
+const alphabet = [65, 66, 67];
+ 
+// Keep track of index outside of the event handler.
+let index = 0;
+ 
+// This is the function that would be invoked by the event listener.
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which);
+ 
+  if (key === alphabet[index]) {
+    index++;
+ 
+    if (index === alphabet.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
