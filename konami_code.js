c8fa672f3b764ef839273028b1f914c03ea0bcbb
mathematical-application-3607 <hannamarie.mehmet@wework.com>
2019-03-18 19:47:31 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -3,4 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
 
+const body = document.querySelector('body')
+body.addEventListener('keydown', e => console.log(e.which))
+
 }
