9a9a192f8719a1afdc686bcd4c7448a12ad48e2b
mathematical-application-3607 <hannamarie.mehmet@wework.com>
2019-03-18 20:05:13 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -8,7 +8,8 @@ body.addEventListener('keydown', e => onKeyDownHandler(e))
 
 }
 
- 
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
 // Keep track of index outside of the event handler.
 let index = 0;
  
